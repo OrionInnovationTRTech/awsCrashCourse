f48d22d5a042ee155b199f9519d7f924cd1a0207
Mustafa Onan <mustafa.onan@orioninc.com>
2022-11-24 03:21:47 +0300

    ci/cd pipeline config

--- bin/main.ts
+++ bin/main.ts
@@ -2,18 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { MainStack } from '../lib/main-stack';
-import dotenv from "dotenv";
-import path from "path";
 
-dotenv.config({
-  path: path.resolve(__dirname, "../.env")
-})
 
 const app = new cdk.App();
 new MainStack(app, 'MainStack', {
-  stackName: `${process.env.CDK_ENVIRONMENT_NAME}-${process.env.CDK_APP_NAME}`,
+  stackName: `${process.env.ENVIRONMENT_NAME}-${process.env.CDK_APP_NAME}`,
   env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION 
+    account: process.env.AWS_ACCOUNT_ID,
+    region: process.env.AWS_REGION 
   }
 });
